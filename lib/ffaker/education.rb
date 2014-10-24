f1871b64e07f2a2cb248b309c3f8a750a9c5f69c
Luke Steensen <luke.steensen@gmail.com>
2014-10-24 00:55:27 -0500

    add more major adjectives

--- lib/ffaker/education.rb
+++ lib/ffaker/education.rb
@@ -39,7 +39,7 @@ module Faker
 
     DEGREE_SHORT_PREFIX = k %w(AB BS BSc MA MD DMus DPhil)
     DEGREE_PREFIX = k ['Bachelor of Science', 'Bachelor of Arts', 'Master of Arts', 'Doctor of Medicine', 'Bachelor of Music', 'Doctor of Philosophy']
-    MAJOR_ADJ = k(%w(Business Systems Industrial Medical Financial Marketing Political Social) + ['Human Resource'])
+    MAJOR_ADJ = k(%w(Applied Agricultural Computer Legal Military Visual Natural Multidisciplinary Cultural International Business Systems Industrial Medical Financial Marketing Political Social) + ['Human Resource'])
     SCHOOL_PREFIX = k %w(Green South North Wind Lake Hill Lark River Red White)
     SCHOOL_ADJ = k %w(International Global Polytechnic National)
     SCHOOL_TYPE = k %w(School University College Institution Academy)
